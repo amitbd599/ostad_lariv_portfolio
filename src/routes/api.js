d2523384e5944575a0f1a713c812af122f44f6e0
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-08-01 09:52:08 +0600

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -19,52 +19,108 @@ router.get("/logout", middlewares, userController.logout);
 router.put("/update", middlewares, userController.update);
 
 //! Experience
-router.post("/create-experience", experienceController.createExperience);
+router.post(
+  "/create-experience",
+  middlewares,
+  experienceController.createExperience
+);
 router.get("/all-experience", experienceController.allExperience);
 router.get("/single-experience/:id", experienceController.singleExperience);
-router.put("/update-experience/:id", experienceController.updateExperience);
-router.delete("/delete-experience/:id", experienceController.deleteExperience);
+router.put(
+  "/update-experience/:id",
+  middlewares,
+  experienceController.updateExperience
+);
+router.delete(
+  "/delete-experience/:id",
+  middlewares,
+  experienceController.deleteExperience
+);
 
 //! Education
-router.post("/create-education", educationController.createEducation);
+router.post(
+  "/create-education",
+  middlewares,
+  educationController.createEducation
+);
 router.get("/all-education", educationController.allEducation);
 router.get("/single-education/:id", educationController.singleEducation);
-router.put("/update-education/:id", educationController.updateEducation);
-router.delete("/delete-education/:id", educationController.deleteEducation);
+router.put(
+  "/update-education/:id",
+  middlewares,
+  educationController.updateEducation
+);
+router.delete(
+  "/delete-education/:id",
+  middlewares,
+  educationController.deleteEducation
+);
 
 //! Advantage
-router.post("/create-advantage", advantageController.createAdvantage);
+router.post(
+  "/create-advantage",
+  middlewares,
+  advantageController.createAdvantage
+);
 router.get("/all-advantage", advantageController.allAdvantage);
 router.get("/single-advantage/:id", advantageController.singleAdvantage);
-router.put("/update-advantage/:id", advantageController.updateAdvantage);
-router.delete("/delete-advantage/:id", advantageController.deleteAdvantage);
+router.put(
+  "/update-advantage/:id",
+  middlewares,
+  advantageController.updateAdvantage
+);
+router.delete(
+  "/delete-advantage/:id",
+  middlewares,
+  advantageController.deleteAdvantage
+);
 
 //! Portfolio
-router.post("/create-portfolio", portfolioController.createPortfolio);
+router.post(
+  "/create-portfolio",
+  middlewares,
+  portfolioController.createPortfolio
+);
 router.get("/all-portfolio", portfolioController.allPortfolio);
 router.get("/single-portfolio/:id", portfolioController.singlePortfolio);
-router.put("/update-portfolio/:id", portfolioController.updatePortfolio);
-router.delete("/delete-portfolio/:id", portfolioController.deletePortfolio);
+router.put(
+  "/update-portfolio/:id",
+  middlewares,
+  portfolioController.updatePortfolio
+);
+router.delete(
+  "/delete-portfolio/:id",
+  middlewares,
+  portfolioController.deletePortfolio
+);
 
 //! Service
-router.post("/create-service", serviceController.createService);
+router.post("/create-service", middlewares, serviceController.createService);
 router.get("/all-service", serviceController.allService);
 router.get("/single-service/:id", serviceController.singleService);
-router.put("/update-service/:id", serviceController.updateService);
-router.delete("/delete-service/:id", serviceController.deleteService);
+router.put("/update-service/:id", middlewares, serviceController.updateService);
+router.delete(
+  "/delete-service/:id",
+  middlewares,
+  serviceController.deleteService
+);
 
 //! Blog
-router.post("/create-blog", blogController.createBlog);
+router.post("/create-blog", middlewares, blogController.createBlog);
 router.get("/all-blog/:perPage/:pageNo", blogController.allBlog);
 router.get("/single-blog/:id", blogController.singleBlog);
-router.put("/update-blog/:id", blogController.updateBlog);
-router.delete("/delete-blog/:id", blogController.deleteBlog);
+router.put("/update-blog/:id", middlewares, blogController.updateBlog);
+router.delete("/delete-blog/:id", middlewares, blogController.deleteBlog);
 
 //! Comment
 router.post("/create-comment", commentController.createComment);
 router.get("/all-comment", commentController.allComment);
 router.get("/single-comment/:id", commentController.singleComment);
-router.put("/update-comment/:id", commentController.updateComment);
-router.delete("/delete-comment/:id", commentController.deleteComment);
+router.put("/update-comment/:id", middlewares, commentController.updateComment);
+router.delete(
+  "/delete-comment/:id",
+  middlewares,
+  commentController.deleteComment
+);
 
 module.exports = router;
