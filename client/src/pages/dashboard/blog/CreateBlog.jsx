e504281694f7f562eac84e033989904d424b1de9
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-09-07 16:40:13 +0600

    add data

--- client/src/pages/dashboard/blog/CreateBlog.jsx
+++ client/src/pages/dashboard/blog/CreateBlog.jsx
@@ -1,6 +1,126 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 import DashboardLayout from "../../../layout/DashboardLayout";
+import BlogStore from "../../../store/BlogStore";
 
 const CreateBlog = () => {
+  let { createBlogLoading, createBlogRequest } = BlogStore();
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+
+  let [data, setData] = useState({
+    title: "",
+    category: "",
+    img: "",
+    sortDescription: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent page reload
+
+    data.description = value;
+    let res = await createBlogRequest(data);
+    if (res) {
+      navigate("/get-all-blog");
+    }
+  };
+
+  console.log(value);
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color",
+    "video",
+    "background",
+    "clean",
+    "code",
+    "align",
+    "direction",
+    "code-block",
+  ];
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [
+        {
+          color: [
+            "#000000",
+            "#be0027",
+            "#cf8d2e",
+            "#e4e932",
+            "#2c9f45",
+            "#371777",
+            "#511378",
+            "#ff0000",
+            "#52565e",
+            "#f3f4f7",
+            "#00aeff",
+            "#ff4f81",
+            "#2dde98",
+            "#0389ff",
+          ],
+        },
+        {
+          background: [
+            "#000000",
+            "#ffffff",
+            "#be0027",
+            "#cf8d2e",
+            "#e4e932",
+            "#2c9f45",
+            "#371777",
+            "#511378",
+            "#ff0000",
+            "#52565e",
+            "#f3f4f7",
+            "#00aeff",
+            "#ff4f81",
+            "#2dde98",
+            "#0389ff",
+          ],
+        },
+      ],
+
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      [
+        { align: "" },
+        { align: "center" },
+        { align: "right" },
+        { align: "justify" },
+      ],
+      ["link", "image", "video"],
+      ["code", "clean", "code-block"],
+    ],
+  };
+
   return (
     <DashboardLayout>
       <section className='mt-[50px]'>
@@ -12,7 +132,7 @@ const CreateBlog = () => {
               </div>
 
               <form
-                id='contact-form'
+                onSubmit={handleSubmit}
                 className='contact-form aos-init aos-animate'
                 data-aos='fade-up'
                 data-aos-delay={100}
@@ -24,6 +144,8 @@ const CreateBlog = () => {
                     placeholder='Title'
                     required
                     type='text'
+                    onChange={handleChange}
+                    value={data?.title}
                   />
                 </div>
                 <div className='mt-[30px]'>
@@ -33,6 +155,8 @@ const CreateBlog = () => {
                     placeholder='Category'
                     required
                     type='text'
+                    onChange={handleChange}
+                    value={data?.category}
                   />
                 </div>
                 <div className='mt-[30px]'>
@@ -42,31 +166,44 @@ const CreateBlog = () => {
                     placeholder='Sort Description'
                     required
                     type='text'
+                    onChange={handleChange}
+                    value={data?.sortDescription}
                   />
                 </div>
                 <div className='mt-[30px]'>
-                  <textarea
-                    name='longDescription'
-                    className='inputBox'
-                    placeholder='Long Description'
-                    required
-                    type='text'
+                  <ReactQuill
+                    theme='snow'
+                    modules={modules}
+                    formats={formats}
+                    value={value}
+                    onChange={setValue}
                   />
                 </div>
                 <div className='mt-[30px]'>
                   <input
-                    name='featureImg'
+                    name='img'
                     className='inputBox'
                     placeholder='Feature Img CDN'
                     required
                     type='text'
+                    onChange={handleChange}
+                    value={data?.img}
                   />
                 </div>
                 <div
                   className='wow fadeIn  animated mt-[30px]'
                   style={{ visibility: "visible", animationName: "fadeIn" }}
                 >
-                  <button className='btn'>Create Blog</button>
+                  {" "}
+                  <button className='btn ' type='submit'>
+                    {createBlogLoading ? (
+                      <span className='flex gap-2'>
+                        <HiOutlineRefresh /> Processing
+                      </span>
+                    ) : (
+                      <>Create Blog</>
+                    )}
+                  </button>
                 </div>
               </form>
             </div>
---
+++ client/src/store/BlogStore.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { create } from "zustand";
+import { baseURL } from "../helper/config";
+import { ErrorToast, SuccessToast } from "../helper/helper";
+
+const BlogStore = create((set) => ({
+  //! create-blog api -- done
+  createBlogLoading: false,
+  createBlogRequest: async (reqBody) => {
+    try {
+      set({ createBlogLoading: true });
+      let res = await axios.post(baseURL + "/create-blog", reqBody, {
+        withCredentials: true,
+      });
+      if (res?.data?.success === true) {
+        SuccessToast(res?.data?.message);
+        set({ createBlogLoading: false });
+        return true;
+      } else {
+        ErrorToast(res?.data?.message);
+        set({ createBlogLoading: false });
+        return false;
+      }
+    } catch (e) {
+      console.log(e);
+      set({ createBlogLoading: false });
+      return false;
+    }
+  },
+
+  //! all-blog api -- done
+  allBlogData: null,
+  allBlogRequest: async () => {
+    try {
+      let res = await axios.get(baseURL + "/all-blog/10/1", {
+        withCredentials: true,
+        credentials: "include",
+      });
+      if (res?.data?.success === true) {
+        set({ allBlogData: res?.data?.data?.blogs });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  //! delete-blog api
+  deleteBlogRequest: async (id) => {
+    try {
+      let res = await axios.delete(baseURL + "/delete-blog/" + id, {
+        withCredentials: true,
+      });
+      if (res?.data?.success === true) {
+        SuccessToast(res?.data?.message);
+        return true;
+      } else {
+        ErrorToast(res?.data?.message);
+        return false;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  //! single-blog -- done
+  singleBlogData: null,
+  singleBlogRequest: async (id) => {
+    try {
+      let res = await axios.get(baseURL + "/single-blog/" + id, {
+        withCredentials: true,
+      });
+      if (res?.data?.success === true) {
+        set({ singleBlogData: res?.data?.data });
+        return true;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  //! update-blog api -- done
+  updateBlogLoading: false,
+  updateBlogRequest: async (reqBody, id) => {
+    try {
+      set({ updateBlogLoading: true });
+      let res = await axios.put(baseURL + "/update-blog/" + id, reqBody, {
+        withCredentials: true,
+      });
+      if (res?.data?.success === true) {
+        SuccessToast(res?.data?.message);
+        set({ updateBlogLoading: false });
+        return true;
+      } else {
+        ErrorToast(res?.data?.message);
+        set({ updateBlogLoading: false });
+        return false;
+      }
+    } catch (e) {
+      console.log(e);
+      set({ updateBlogLoading: false });
+      return false;
+    }
+  },
+}));
+
+export default BlogStore;
